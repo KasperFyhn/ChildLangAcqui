041349ac737b2cc6e3bf10b67052399850fcd22d
Kasper Fyhn Jacobsen <kasperfyhnjacobsen@gmail.com>
2018-10-12 13:50:41 +0200

    tested a few things

--- temp.py
+++ temp.py
@@ -2,4 +2,4 @@ import transcripts as ts
 
 transcripts = ts.load_all_from_dir('/Users/kasperfyhnjacobsen/Desktop/Brown/Adam')
 
-ts.plot_prop_word_freqs(['i', 'mom'], transcripts)
+ts.plot_prop_word_freqs(['i', 'you'], transcripts)
